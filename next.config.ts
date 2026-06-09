684b2472836e485650b575fc3a16582029215a60
integr8-dev-ralph <rhontiveros@integr8.com.ph>
2026-06-09 09:46:06 +0800

    FIX: Turbopack configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	poweredByHeader: false,
 	allowedDevOrigins: ["192.168.18.190"],
+	turbopack: {
+		root: process.cwd(),
+	},
 	experimental: {
 		viewTransition: true,
 		staleTimes: {
